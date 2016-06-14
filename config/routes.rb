0c39ee2181f348dcf8b3bb9dad4f2cd15713fd58
kuniyuki kikuchi <kkikuchi@netprotections.co.jp>
2016-06-14 04:17:56 +0000

    add sign sign up and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'signup',  to: 'users#new'
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
+  
   # get 'static_pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
