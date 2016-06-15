bd1acfd8c129a8688f0080dee866c3ec33e66b09
kuniyuki kikuchi <kkikuchi@netprotections.co.jp>
2016-06-15 06:19:40 +0000

    add home view's timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 before_action :authenticate_user!, only: [:edit, :update]
 
   def show
+    p params[:id]
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
   end
